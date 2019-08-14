2c231a5b450b44c59027135fc8e64c4d2b1a15ca
johan villamil <johinsdev@gmail.com>
2019-08-13 22:53:16 -0500

    add husky

--- jest.test.js
+++ jest.test.js
@@ -1,4 +1,4 @@
-const { sum, subtract, sumAsync, subtractAsync } = require("./math");
+const { sumAsync, subtractAsync } = require("./math");
 
 test("sumAsync adds numbers asynchronously", async () => {
   const result = await sumAsync(3, 7);
@@ -8,18 +8,6 @@ test("sumAsync adds numbers asynchronously", async () => {
 
 test("subtractAsync subtracts numbers asynchronously", async () => {
   const result = await subtractAsync(7, 3);
-  const expected = 4;
-  expect(result).toBe(expected);
-});
-
-test("sum adds numbers", () => {
-  const result = sum(3, 7);
-  const expected = 10;
-  expect(result).toBe(expected);
-});
-
-test("subtract subtracts numbers", () => {
-  const result = subtract(7, 3);
   const expected = 4;
   expect(result).toBe(expected);
 });
